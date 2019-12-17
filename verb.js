ded739180f1c4d50731534bac8f4dc774d7c3f00
jneill2 <43153570+jneill2@users.noreply.github.com>
2019-12-17 16:07:47 -0500

    Add files via upload

---
+++ verb.js
@@ -0,0 +1,702 @@
+var lyw = {
+    present: {
+        active: {
+            infinitive: 'λύειν',
+            indicative: [
+                ['λύω', 'λύομεν'],
+                ['λύεις', 'λύετε'],
+                ['λύει', 'λύουσιν || λύουσι']
+            ],
+            subjunctive: [
+                ['λύω', 'λύωμεν'],
+                ['λύῃς', 'λύητε'],
+                ['λύῃ', 'λύωσιν || λύωσι']
+            ],
+            optative: [
+
+            ],
+            participle: [
+
+            ]
+        },
+        middle: {
+            infinitive: 'λύεσθαι',
+            indicative: [
+                ['λύομαι', 'λυόμεθα'],
+                ['λύου', 'λύεσθε'],
+                ['λύεται', 'λύονται']
+            ],
+            subjunctive: [
+                ['λύωμαι', 'λυώμεθα'],
+                ['λύῃ', 'λύησθε'],
+                ['λύηται', 'λύωνται']
+            ],
+            optative: [
+                
+            ],
+            participle: [
+
+            ]
+        },
+        passive: {
+            infinitive: 'λῦσαι',
+            indicative: [
+                ['λύω', 'λύομεν'],
+                ['λύεις', 'λύετε'],
+                ['λύει', 'λύουσιν || λύουσι']
+            ],
+            subjunctive: [
+                ['λύω', 'λύωμεν'],
+                ['λύῃς', 'λύητε'],
+                ['λύῃ', 'λύωσιν || λύωσι']
+            ],
+            optative: [
+                
+            ],
+            participle: [
+
+            ]
+        },
+    },
+    aorist: {
+        active: {
+            infinitive: 'λῦσαι',
+            indicative: [
+                ['ἔλυσα', 'ἐλύσαμεν'],
+                ['ἔλυσας', 'ἐλύσατε'],
+                ['ἔλυσεν', 'ἔλυσαν']
+            ],
+            subjunctive: [
+                ['λύσω', 'λύσωμεν'],
+                ['λύσῃς', 'λύσητε'],
+                ['λύσῃ', 'λύσωσιν || λύσωσι']
+            ],
+            optative: [
+
+            ],
+            participle: [
+
+            ]
+        },
+        middle: {
+            infinitive: 'λύσασθαι',
+            indicative: [
+                ['ἐλυσάμην', 'ἐλυσάμεθα'],
+                ['ἐλύσω', 'ἐλύσασθαι'],
+                ['ἐλύσατο', 'ἐλύσαντο']
+            ],
+            subjunctive: [
+                ['λύσωμαι', 'λυσώμεθα'],
+                ['λύσῃ', 'λύσησθε'],
+                ['λύσηται', 'λύσωνται']
+            ],
+            optative: [
+                
+            ],
+            participle: [
+
+            ]
+        },
+        passive: {
+            infinitive: 'λυθῆναι',
+            indicative: [
+                ['ἐλύθην', 'ἐλύθημεν'],
+                ['ἐλύθης', 'ἐλύθητε'],
+                ['ἐλύθη', 'ἐλύθησαν']
+            ],
+            subjunctive: [
+                ['λύθω', 'λύθημεν'],
+                ['λύθῃς', 'λύθητε'],
+                ['λύθῃ', 'λύθωσιν || λύθωσι']
+            ],
+            optative: [
+                
+            ],
+            participle: [
+
+            ]
+        },
+    },
+    perfect: {
+        active: {
+
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    examples: [
+
+    ]
+}
+var exv = {
+    present: {
+        active: {
+            infinitive: 'ἔχειν',
+            indicative: [
+                ['ἔχω','ἔχομεν'],
+                ['ἔχεις','ἔχετε'],
+                ['ἔχει','ἔχουσιν || ἔχουσι']
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var grafv = {
+    present: {
+        active: {
+            infinitive: 'γράφειν',
+            indicative: [
+                ['γράφω','γράφομεν'],
+                ['γράφεις','γράφετε'],
+                ['γράφει','γράφουσιν || γράφουσι']
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var divkv = {
+    present: {
+        active: {
+            infinitive: 'διώκειν',
+            indicative: [
+                ['διώκω','διώκομεν'],
+                ['διώκεις','διώκετε'],
+                ['διώκει','διώκουσιν || διώκουσι']
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var feygv = {
+    present: {
+        active: {
+            infinitive: 'φεύγειν',
+            indicative: [
+                ['φεύγω','φεύγομεν'],
+                ['φεύγεις','φεύγετε'],
+                ['φεύγει','φεύγουσιν || φεύγουσι']
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var peripatev = {
+    present: {
+        active: {
+            infinitive: 'περιπατεῖν',
+            indicative: [
+                ['περιπατῶ','περιπατοῦμεν'],
+                ['περιπατεῖς','περιπατεῖτε'],
+                ['περιπατεῖ','περιπατοῦσιν || περιπατοῦσι']
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+
+var erxomai = {
+    present: {
+        active: {
+            infinitive: '',
+            indicative: [
+            ]
+        },
+        middle: {
+            infinitive: 'ἔρχεσθαι',
+            indicative: [
+                ['ἔρχομαι','ἐρχόμεθα'],
+                ['ἔρχῃ','ἔρχεσθε'],
+                ['ἔρχεται','ἔρχονται']
+            ]
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var kauhmai = {
+    present: {
+        active: {
+            infinitive: '',
+            indicative: [
+            ]
+        },
+        middle: {
+            infinitive: 'κάθησθαι',
+            indicative: [
+                ['κάθημαι','καθήμεθα'],
+                ['κάθησαι','κάθησθε'],
+                ['κάθηται','κάθηνται']
+            ]
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var proseyxomai = {
+    present: {
+        active: {
+            infinitive: '',
+            indicative: [
+            ]
+        },
+        middle: {
+            infinitive: 'προσεύχεσθαι',
+            indicative: [
+                ['προσεύχομαι','προσευχόμεθα'],
+                ['προσεύχῃ','προσεύχεσθε'],
+                ['προσεύχεται','προσεύχονται']
+            ]
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var egeiromai = {
+    present: {
+        active: {
+            infinitive: '',
+            indicative: [
+            ]
+        },
+        middle: {
+            infinitive: 'ἐγείρεσθαι',
+            indicative: [
+                ['ἐγείρομαι','ἐγειρόμεθα'],
+                ['ἐγείρῃ','ἐγείεσθε'],
+                ['ἐγείρεται','ἐγείρονται']
+            ]
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+
+var tiuhmi = {
+    present: {
+        active: {
+            infinitive: 'τιθέναι',
+            indicative: [
+                ['τίθημι','τίθεμεν'],
+                ['τίθης','τίθετε'],
+                ['τίθησιν || τίθησι','τιθέασιν || τιθέασι']
+            ]
+        },
+        middle: {
+            infinitive: '',
+            indicative: [
+            ]
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var didvmi = {
+    present: {
+        active: {
+            infinitive: 'διδόναι',
+            indicative: [
+                ['δίδωμι','δίδομεν'],
+                ['δίδως','δίδοτε'],
+                ['δίδωσιν || δίδωσι','διδόασιν || διδόασι']
+            ]
+        },
+        middle: {
+            infinitive: '',
+            indicative: [
+            ]
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
+var deiknymi = {
+    present: {
+        active: {
+            infinitive: '',
+            indicative: [
+                ['δείκνυμι','δείκνυμεν'],
+                ['δείκνυς','δείκνυτε'],
+                ['δείκνυσιν || δείκνυσι','δεικύουσιν || δεικύασι']
+            ]
+        },
+        middle: {
+            infinitive: '',
+            indicative: [
+            ]
+        },
+        passive: {
+            
+        }
+    },
+    aorist: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    },
+    perfect: {
+        active: {
+            infinitive: '',
+            indicative: [
+                
+            ]
+        },
+        middle: {
+
+        },
+        passive: {
+
+        }
+    }
+}
